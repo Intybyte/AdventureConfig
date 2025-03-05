c31b0499bbd1792620b734db3ee030ef459b00e8
Intybyte <vaan.testwork@gmail.com>
2025-03-05 17:05:24 +0100

    Add javadocs + fixes

--- src/main/java/me/vaan/adventureconfig/Message.java
+++ src/main/java/me/vaan/adventureconfig/Message.java
@@ -13,17 +13,47 @@ import java.util.List;
 
 public class Message {
     private static final Duration TICK = Duration.ofMillis(50);
+    private final String keyPrefix;
+    private final Component beforeMessage;
 
-    public static void sendMessage(Audience sender, String key) {
-        Config cs = Config.getInstance();
-        Component prefix = cs.getComponent("messages.prefix");
-
-        sender.sendMessage(prefix.append(cs.getComponent("messages." + key)));
+    public Message() {
+        this.keyPrefix = "";
+        this.beforeMessage = Component.empty();
     }
 
-    public static void sendMessage(Audience player, String key, String... arg) {
+    public Message(String keyPrefix) {
+        this.keyPrefix = keyPrefix + ".";
+        this.beforeMessage = Component.empty();
+    }
+
+    public Message(String keyPrefix, Component beforeMessage) {
+        this.keyPrefix = keyPrefix + ".";
+        this.beforeMessage = beforeMessage;
+    }
+
+    /**
+     * Send a simple message to the user without any arguments to process.
+     *
+     * @param audience The target that must see the message
+     * @param key Combined with the prefix key to obtain data from Config
+     */
+    public void sendMessage(Audience audience, String key) {
         Config cs = Config.getInstance();
-        Component prefix = cs.getComponent("messages.prefix");
+        audience.sendMessage(beforeMessage.append(cs.getComponent(keyPrefix + key)));
+    }
+
+    /**
+     * This method processes the args given based on the position:
+     * The first arg will replace any occurrences of &lt;arg1&gt;
+     * The second arg will replace any occurrences of &lt;arg2&gt;
+     * And so forth depending on the number of arguments passed.
+     *
+     * @param audience The target that must see the message
+     * @param key Combined with the prefix key to obtain data from Config
+     * @param arg List of arguments to process on the tag
+     */
+    public void sendMessage(Audience audience, String key, String... arg) {
+        Config cs = Config.getInstance();
 
         TagResolver[] resolvers = new TagResolver[arg.length + 1];
         resolvers[arg.length] = TagResolver.standard();
@@ -34,20 +64,27 @@ public class Message {
         TagResolver total = TagResolver.resolver(resolvers);
         MiniMessage mm = MiniMessage.builder().tags(total).build();
 
-        Component componentMessage = mm.deserialize(cs.getString("messages." + key));
-        player.sendMessage(prefix.append(componentMessage));
+        Component componentMessage = mm.deserialize(cs.getString(keyPrefix + key));
+        audience.sendMessage(beforeMessage.append(componentMessage));
     }
 
-    public static void sendTitle(Audience player, String key) {
+    /**
+     * This method doesn't use beforeMessage defined before.
+     * In the config file these tags must be defined in order to use in this method:
+     * <br>
+     * titles.fade_in
+     * titles.stay_duration
+     * titles.fade_out
+     *
+     * @param audience The target that must see the title
+     * @param key Combined with the prefix key to obtain data from Config
+     */
+    public void sendTitle(Audience audience, String key) {
         Config cs = Config.getInstance();
-        if (!cs.getBool("titles.enabled")) {
-            return;
-        }
-
         MiniMessage mm = MiniMessage.miniMessage();
 
         List<Component> cmp = new ArrayList<>();
-        for (String line : cs.getSList("messages." + key)) {
+        for (String line : cs.getSList(keyPrefix + key)) {
             cmp.add(mm.deserialize(line));
         }
 
@@ -67,6 +104,6 @@ public class Message {
             title = Title.title(cmp.get(0), cmp.get(1), times);
         }
 
-        player.showTitle(title);
+        audience.showTitle(title);
     }
 }
