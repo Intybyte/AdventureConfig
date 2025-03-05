51876e243b3169b3a1a78d98f93e97132e5c2c77
Intybyte <vaan.testwork@gmail.com>
2025-03-05 16:53:58 +0100

    First commit

---
+++ src/main/java/me/vaan/adventureconfig/Config.java
@@ -0,0 +1,107 @@
+package me.vaan.adventureconfig;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Config {
+
+    private static Config instance = null;
+    public static Config getInstance() {
+        return instance;
+    }
+
+    private final File file;
+
+    private final Map<String, Object> configMap = new HashMap<>();
+    private final Map<String, Component> componentMap = new HashMap<>();
+    private static final MiniMessage mm = MiniMessage.miniMessage();
+
+    public static void init(File file) {
+        instance = new Config(file);
+    }
+
+    private Config(File config) {
+        this.file = config;
+        try (FileInputStream fis = new FileInputStream(config)) {
+            Yaml yaml = new Yaml();
+
+            Map<String, Object> map = yaml.load(fis);
+            deepLoadKeys("", map);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading config file: " + e.getMessage());
+        }
+    }
+
+    public void reloadConfig() {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            Yaml yaml = new Yaml();
+            configMap.clear();
+            componentMap.clear();
+
+            Map<String, Object> map = yaml.load(fis);
+            deepLoadKeys("", map);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error reloading config file: " + e.getMessage());
+        }
+    }
+
+    public Object get(String key) {
+        Object value = configMap.get(key);
+        if (value == null) {
+            throw new RuntimeException("Error: " + key + " not found.");
+        }
+
+        return value;
+    }
+
+    public String getString(String key) {
+        return (String) get(key);
+    }
+
+    public List<String> getSList(String key) {
+        return (List<String>) get(key);
+    }
+
+    public int getInt(String key) {
+        return (Integer) get(key);
+    }
+
+    public boolean getBool(String key) {
+        return (Boolean) get(key);
+    }
+
+    // don't deserialize stuff again if it has no argument to evaluate
+    public Component getComponent(String key) {
+        Component cmp = componentMap.get(key);
+        if (cmp != null) {
+            return cmp;
+        }
+
+        cmp = mm.deserialize(getString(key));
+        componentMap.put(key, cmp);
+        return cmp;
+    }
+
+    // Recursive method to deep load all keys
+    private void deepLoadKeys(String prefix, Map<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String key = prefix.isEmpty() ? entry.getKey() : prefix + "." + entry.getKey();  // Create a "dot" notation for keys
+            Object value = entry.getValue();
+
+            if (value instanceof Map) {
+                deepLoadKeys(key, (Map<String, Object>) value);
+            } else {
+                configMap.put(key, value);
+            }
+        }
+    }
+}
---
+++ src/main/java/me/vaan/adventureconfig/Message.java
@@ -0,0 +1,72 @@
+package me.vaan.adventureconfig;
+
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.title.Title;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Message {
+    private static final Duration TICK = Duration.ofMillis(50);
+
+    public static void sendMessage(Audience sender, String key) {
+        Config cs = Config.getInstance();
+        Component prefix = cs.getComponent("messages.prefix");
+
+        sender.sendMessage(prefix.append(cs.getComponent("messages." + key)));
+    }
+
+    public static void sendMessage(Audience player, String key, String... arg) {
+        Config cs = Config.getInstance();
+        Component prefix = cs.getComponent("messages.prefix");
+
+        TagResolver[] array = new TagResolver[arg.length + 1];
+        array[arg.length] = TagResolver.standard();
+        for (int i = 0; i < arg.length; i++) {
+            TagResolver targetResolver = TagResolver.resolver("arg" + i, Tag.selfClosingInserting(Component.text(arg[i])));
+        }
+
+        TagResolver total = TagResolver.resolver(array);
+        MiniMessage mm = MiniMessage.builder().tags(total).build();
+
+        Component componentMessage = mm.deserialize(cs.getString("messages." + key));
+        player.sendMessage(prefix.append(componentMessage));
+    }
+
+    public static void sendTitle(Audience player, String key) {
+        Config cs = Config.getInstance();
+        if (!cs.getBool("titles.enabled")) {
+            return;
+        }
+
+        MiniMessage mm = MiniMessage.miniMessage();
+
+        List<Component> cmp = new ArrayList<>();
+        for (String line : cs.getSList("messages." + key)) {
+            cmp.add(mm.deserialize(line));
+        }
+
+        if (cmp.isEmpty()) {
+            return;
+        }
+
+        int fadeIn = cs.getInt("titles.fade_in");
+        int stayDuration = cs.getInt("titles.stay_duration");
+        int fadeOut = cs.getInt("titles.fade_out");
+
+        Title.Times times = Title.Times.times(TICK.multipliedBy(fadeIn), TICK.multipliedBy(stayDuration), TICK.multipliedBy(fadeOut));
+        Title title;
+        if (cmp.size() == 1) {
+            title = Title.title(cmp.get(0), Component.empty(), times);
+        } else {
+            title = Title.title(cmp.get(0), cmp.get(1), times);
+        }
+
+        player.showTitle(title);
+    }
+}
